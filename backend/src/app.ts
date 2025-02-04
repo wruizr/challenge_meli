466fab644d97d59794109533adea87550a25d0e2
Wilson Ruiz <ruizwilson01@gmail.com>
2025-02-04 11:18:15 -0500

    backend: Se agrega server.ts y .env separando responsabilidades y agregando variables de entorno.

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express, { Express } from 'express'
 import cors from 'cors'
 import router from './routes'
+
 const app: Express = express()
 
 app.use(cors())
@@ -8,6 +9,4 @@ app.use(express.json())
 
 app.use('/api', router)
 
-app.listen(3000, () => {
-    console.log(`Servidor corriendo en el puerto http://localhost:${3000}`)
-})
+export default app
---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import app from './app'
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto http://localhost:${PORT}`)
+})
