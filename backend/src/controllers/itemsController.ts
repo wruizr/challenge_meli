50af77fc87f62441f4886a1277ca1040faddb6bf
Wilson Ruiz <ruizwilson01@gmail.com>
2025-02-04 17:55:35 -0500

    backend: Se agragan servicios, controllers y utils, y se modifica routes.

---
+++ backend/src/controllers/itemsController.ts
@@ -0,0 +1,87 @@
+import { Request, Response } from 'express'
+import { getItemDetail, searchItems } from '../services/meliService'
+import { Author, GenerateTitleResponse, Item, ItemDetail, ItemDetailResponse, ItemsResponse } from '../interfaces/apiResponse';
+
+const author: Author = {
+    name: 'Wilson',
+    lastname: 'Ruiz'
+}
+
+export const searchItemsController = async (req: Request, res: Response) => {
+    const { q } = req.query as { q: string }
+    if (!q || typeof q !== 'string') {
+      res.status(400).json({ error: 'El parámetro "q" es requerido y debe ser una cadena.' });
+    }
+    try {
+        const response = await searchItems(q)
+        const items: Item[] = response.results.slice(0, 4).map((item) => ({
+            id: item.id,
+            title: item.title,
+            price: {
+                currency: item.currency_id,
+                amount: item.price,
+                decimals: 2,
+                regular_amount: item.original_price
+            },
+            picture: item.thumbnail,
+            condition: item.condition,
+            free_shipping: item.shipping.free_shipping
+        }))
+        const itemsResponse: ItemsResponse = {
+            author,
+            items,
+          };
+        res.json(itemsResponse);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener los resultados.'})
+    }
+}
+
+export const getItemDetailController = async (req: Request, res: Response) => {
+    const { id } = req.params;
+  
+    try {
+      const {itemResponse, descriptionResponse, sellerResponse} = await getItemDetail(id)
+  
+      const item: ItemDetail = {
+        id: itemResponse.id,
+        title: itemResponse.title,
+        price: {
+            currency: itemResponse.currency_id,
+            amount: itemResponse.price,
+            decimals: 2,
+        },
+        picture: itemResponse.pictures[0].url,
+        condition: itemResponse.condition,
+        free_shipping: itemResponse.shipping.free_shipping,
+        sold_quantity: sellerResponse.seller_reputation.transactions.total,
+        description: descriptionResponse.plain_text,
+    }
+  
+      const response: ItemDetailResponse = {
+        author,
+        item: item,
+      };
+  
+      res.json(response);
+    } catch (error) {
+      res.status(500).json({  error: 'Error al obtener el resultado.' });
+    }
+  };
+
+  export const generateTitleController = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const title = `Título generado para el item ${id}`;
+  
+      const response: GenerateTitleResponse = {
+        author,
+        title,
+        useIAIntegration: true,
+      };
+  
+      res.json(response);
+    } catch (error) {
+      res.status(500).json({  error: 'Error al generar el resultado.' });
+    }
+  };
--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express'
+import { searchItemsController, getItemDetailController, generateTitleController } from '../controllers/itemsController'
 
 const router = Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello World')
-}) 
+router.get('/items', searchItemsController)
+router.get('/items/:id', getItemDetailController)
+router.get('/generate-title/:id', generateTitleController)
 
 export default router
