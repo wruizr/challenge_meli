44563f1c4b9b3a342aae1ca6b9d726e8ef058737
Wilson Ruiz <ruizwilson01@gmail.com>
2025-02-04 11:12:46 -0500

    backend: Se realiza configuracion inicial de express

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,1 +1,13 @@
-console.log("Hola Mundo!")
+import express, { Express } from 'express'
+import cors from 'cors'
+import router from './routes'
+const app: Express = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api', router)
+
+app.listen(3000, () => {
+    console.log(`Servidor corriendo en el puerto http://localhost:${3000}`)
+})
