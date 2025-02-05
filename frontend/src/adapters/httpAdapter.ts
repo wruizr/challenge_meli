8561477e54aae9070d77f8ededb172ec43e5c5ea
Wilson Ruiz <ruizwilson01@gmail.com>
2025-02-04 22:04:10 -0500

    frontend: Se agrega adaptador

---
+++ frontend/src/adapters/httpAdapter.ts
@@ -0,0 +1,23 @@
+import axios, { AxiosInstance, AxiosResponse } from 'axios'
+import { handleError } from '../utils/errorHandler'
+
+class HttpAdapter {
+    private axiosInstance: AxiosInstance
+
+    constructor(baseURL: string) {
+        this.axiosInstance = axios.create({
+            baseURL,
+        })
+    }
+
+    async get<T>(url: string): Promise<T> {
+        try {
+            const response: AxiosResponse<T> = await this.axiosInstance.get(url)
+            return response.data
+        } catch (error) {
+            throw handleError(error, 'HTTP GET solicitud fallida')
+        }
+    }
+}
+
+export default HttpAdapter
